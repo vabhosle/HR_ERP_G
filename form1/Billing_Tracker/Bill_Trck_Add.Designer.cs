9f244305d6ec7681a6f9dd14db85c7a0f753991c
PRATHAMESH <prathamesh6012@gmail.com>
2014-07-10 13:23:19 +0530

    billing tracker added

---
+++ form1/Billing_Tracker/Bill_Trck_Add.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace form1
+{
+    partial class bil_trck_add_frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bil_add_grb = new System.Windows.Forms.GroupBox();
+            this.bil_clr_add_btn = new System.Windows.Forms.Button();
+            this.bil_addRec_add_btn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bil_dtOfBil_add_lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bil_balInAcc_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_senForPro_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_status_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_bilTow_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_bilAmt_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_dtOfBil_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_venNam_add_txt = new System.Windows.Forms.TextBox();
+            this.bil_dtOfBil_add_dtp = new System.Windows.Forms.DateTimePicker();
+            this.bil_senForPro_add_dtp = new System.Windows.Forms.DateTimePicker();
+            this.invt_cldtFr_srch_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bil_add_grb.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bil_add_grb
+            // 
+            this.bil_add_grb.Controls.Add(this.pictureBox2);
+            this.bil_add_grb.Controls.Add(this.pictureBox1);
+            this.bil_add_grb.Controls.Add(this.button1);
+            this.bil_add_grb.Controls.Add(this.invt_cldtFr_srch_btn);
+            this.bil_add_grb.Controls.Add(this.bil_senForPro_add_dtp);
+            this.bil_add_grb.Controls.Add(this.bil_dtOfBil_add_dtp);
+            this.bil_add_grb.Controls.Add(this.bil_clr_add_btn);
+            this.bil_add_grb.Controls.Add(this.bil_addRec_add_btn);
+            this.bil_add_grb.Controls.Add(this.label7);
+            this.bil_add_grb.Controls.Add(this.label6);
+            this.bil_add_grb.Controls.Add(this.label5);
+            this.bil_add_grb.Controls.Add(this.label4);
+            this.bil_add_grb.Controls.Add(this.label3);
+            this.bil_add_grb.Controls.Add(this.bil_dtOfBil_add_lbl);
+            this.bil_add_grb.Controls.Add(this.label1);
+            this.bil_add_grb.Controls.Add(this.bil_balInAcc_add_txt);
+            this.bil_add_grb.Controls.Add(this.bil_senForPro_add_txt);
+            this.bil_add_grb.Controls.Add(this.bil_status_add_txt);
+            this.bil_add_grb.Controls.Add(this.bil_bilTow_add_txt);
+            this.bil_add_grb.Controls.Add(this.bil_bilAmt_add_txt);
+            this.bil_add_grb.Controls.Add(this.bil_dtOfBil_add_txt);
+            this.bil_add_grb.Controls.Add(this.bil_venNam_add_txt);
+            this.bil_add_grb.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bil_add_grb.Location = new System.Drawing.Point(12, 12);
+            this.bil_add_grb.Name = "bil_add_grb";
+            this.bil_add_grb.Size = new System.Drawing.Size(948, 178);
+            this.bil_add_grb.TabIndex = 0;
+            this.bil_add_grb.TabStop = false;
+            this.bil_add_grb.Text = "Add Bill Record";
+            // 
+            // bil_clr_add_btn
+            // 
+            this.bil_clr_add_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bil_clr_add_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bil_clr_add_btn.Location = new System.Drawing.Point(823, 114);
+            this.bil_clr_add_btn.Name = "bil_clr_add_btn";
+            this.bil_clr_add_btn.Size = new System.Drawing.Size(91, 32);
+            this.bil_clr_add_btn.TabIndex = 15;
+            this.bil_clr_add_btn.Text = "Clear";
+            this.bil_clr_add_btn.UseVisualStyleBackColor = false;
+            this.bil_clr_add_btn.Click += new System.EventHandler(this.bil_clr_add_btn_Click);
+            // 
+            // bil_addRec_add_btn
+            // 
+            this.bil_addRec_add_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bil_addRec_add_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bil_addRec_add_btn.Location = new System.Drawing.Point(686, 115);
+            this.bil_addRec_add_btn.Name = "bil_addRec_add_btn";
+            this.bil_addRec_add_btn.Size = new System.Drawing.Size(119, 31);
+            this.bil_addRec_add_btn.TabIndex = 14;
+            this.bil_addRec_add_btn.Text = "Add Record";
+            this.bil_addRec_add_btn.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Balance in Account";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(683, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Sent for Processing";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Bill Towards";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(683, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Bill Amount";
+            // 
+            // bil_dtOfBil_add_lbl
+            // 
+            this.bil_dtOfBil_add_lbl.AutoSize = true;
+            this.bil_dtOfBil_add_lbl.Location = new System.Drawing.Point(341, 27);
+            this.bil_dtOfBil_add_lbl.Name = "bil_dtOfBil_add_lbl";
+            this.bil_dtOfBil_add_lbl.Size = new System.Drawing.Size(72, 16);
+            this.bil_dtOfBil_add_lbl.TabIndex = 8;
+            this.bil_dtOfBil_add_lbl.Text = "Date of Bill";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Vendor Name";
+            // 
+            // bil_balInAcc_add_txt
+            // 
+            this.bil_balInAcc_add_txt.Location = new System.Drawing.Point(143, 119);
+            this.bil_balInAcc_add_txt.Name = "bil_balInAcc_add_txt";
+            this.bil_balInAcc_add_txt.Size = new System.Drawing.Size(134, 22);
+            this.bil_balInAcc_add_txt.TabIndex = 6;
+            // 
+            // bil_senForPro_add_txt
+            // 
+            this.bil_senForPro_add_txt.Location = new System.Drawing.Point(471, 71);
+            this.bil_senForPro_add_txt.Name = "bil_senForPro_add_txt";
+            this.bil_senForPro_add_txt.ReadOnly = true;
+            this.bil_senForPro_add_txt.Size = new System.Drawing.Size(114, 22);
+            this.bil_senForPro_add_txt.TabIndex = 5;
+            // 
+            // bil_status_add_txt
+            // 
+            this.bil_status_add_txt.Location = new System.Drawing.Point(763, 71);
+            this.bil_status_add_txt.Name = "bil_status_add_txt";
+            this.bil_status_add_txt.Size = new System.Drawing.Size(154, 22);
+            this.bil_status_add_txt.TabIndex = 4;
+            // 
+            // bil_bilTow_add_txt
+            // 
+            this.bil_bilTow_add_txt.Location = new System.Drawing.Point(143, 68);
+            this.bil_bilTow_add_txt.Name = "bil_bilTow_add_txt";
+            this.bil_bilTow_add_txt.Size = new System.Drawing.Size(154, 22);
+            this.bil_bilTow_add_txt.TabIndex = 3;
+            // 
+            // bil_bilAmt_add_txt
+            // 
+            this.bil_bilAmt_add_txt.Location = new System.Drawing.Point(763, 21);
+            this.bil_bilAmt_add_txt.Name = "bil_bilAmt_add_txt";
+            this.bil_bilAmt_add_txt.Size = new System.Drawing.Size(134, 22);
+            this.bil_bilAmt_add_txt.TabIndex = 2;
+            // 
+            // bil_dtOfBil_add_txt
+            // 
+            this.bil_dtOfBil_add_txt.Location = new System.Drawing.Point(471, 24);
+            this.bil_dtOfBil_add_txt.Name = "bil_dtOfBil_add_txt";
+            this.bil_dtOfBil_add_txt.ReadOnly = true;
+            this.bil_dtOfBil_add_txt.Size = new System.Drawing.Size(114, 22);
+            this.bil_dtOfBil_add_txt.TabIndex = 1;
+            // 
+            // bil_venNam_add_txt
+            // 
+            this.bil_venNam_add_txt.Location = new System.Drawing.Point(143, 21);
+            this.bil_venNam_add_txt.Name = "bil_venNam_add_txt";
+            this.bil_venNam_add_txt.Size = new System.Drawing.Size(154, 22);
+            this.bil_venNam_add_txt.TabIndex = 0;
+            // 
+            // bil_dtOfBil_add_dtp
+            // 
+            this.bil_dtOfBil_add_dtp.Location = new System.Drawing.Point(584, 24);
+            this.bil_dtOfBil_add_dtp.Name = "bil_dtOfBil_add_dtp";
+            this.bil_dtOfBil_add_dtp.Size = new System.Drawing.Size(19, 22);
+            this.bil_dtOfBil_add_dtp.TabIndex = 16;
+            this.bil_dtOfBil_add_dtp.ValueChanged += new System.EventHandler(this.bil_dtOfBil_add_dtp_ValueChanged);
+            // 
+            // bil_senForPro_add_dtp
+            // 
+            this.bil_senForPro_add_dtp.Location = new System.Drawing.Point(584, 71);
+            this.bil_senForPro_add_dtp.Name = "bil_senForPro_add_dtp";
+            this.bil_senForPro_add_dtp.Size = new System.Drawing.Size(19, 22);
+            this.bil_senForPro_add_dtp.TabIndex = 17;
+            this.bil_senForPro_add_dtp.ValueChanged += new System.EventHandler(this.bil_senForPro_add_dtp_ValueChanged);
+            // 
+            // invt_cldtFr_srch_btn
+            // 
+            this.invt_cldtFr_srch_btn.BackColor = System.Drawing.SystemColors.Window;
+            this.invt_cldtFr_srch_btn.Location = new System.Drawing.Point(601, 23);
+            this.invt_cldtFr_srch_btn.Name = "invt_cldtFr_srch_btn";
+            this.invt_cldtFr_srch_btn.Size = new System.Drawing.Size(22, 24);
+            this.invt_cldtFr_srch_btn.TabIndex = 18;
+            this.invt_cldtFr_srch_btn.Text = "X";
+            this.invt_cldtFr_srch_btn.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(601, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 24);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::form1.Properties.Resources.url1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(900, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 82;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::form1.Properties.Resources.url1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(279, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 83;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bil_trck_add_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 453);
+            this.Controls.Add(this.bil_add_grb);
+            this.Name = "bil_trck_add_frm";
+            this.Text = "Billing Tracker Add Record";
+            this.bil_add_grb.ResumeLayout(false);
+            this.bil_add_grb.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox bil_add_grb;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label bil_dtOfBil_add_lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox bil_balInAcc_add_txt;
+        private System.Windows.Forms.TextBox bil_senForPro_add_txt;
+        private System.Windows.Forms.TextBox bil_status_add_txt;
+        private System.Windows.Forms.TextBox bil_bilTow_add_txt;
+        private System.Windows.Forms.TextBox bil_bilAmt_add_txt;
+        private System.Windows.Forms.TextBox bil_dtOfBil_add_txt;
+        private System.Windows.Forms.TextBox bil_venNam_add_txt;
+        private System.Windows.Forms.Button bil_clr_add_btn;
+        private System.Windows.Forms.Button bil_addRec_add_btn;
+        private System.Windows.Forms.DateTimePicker bil_dtOfBil_add_dtp;
+        private System.Windows.Forms.DateTimePicker bil_senForPro_add_dtp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button invt_cldtFr_srch_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
